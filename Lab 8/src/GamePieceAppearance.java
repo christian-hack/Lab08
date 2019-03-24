ff13818b0499435cfb4a9527d0aad652ee809fc9
Christian Hackelman <cphackelman@ou.edu>
2019-03-24 15:45:37 -0500

    Completed GamePieceAppearance class

--- Lab 8/src/GamePieceAppearance.java
+++ Lab 8/src/GamePieceAppearance.java
@@ -1,4 +1,20 @@
 
 public class GamePieceAppearance {
 
+	private Color color;
+	private Shape shape;
+	
+	public GamePieceAppearance(Color color1, Shape shape1) {
+		this.color = color1;
+		this.shape = shape1;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public Shape getShape() {
+		return shape;
+	}
+	
 }
