741037533397659e75279776a9ad2f56c6f22af0
Christian Hackelman <cphackelman@ou.edu>
2019-03-24 15:27:23 -0500

    Completed Color enumeration

--- Lab 8/src/Color.java
+++ Lab 8/src/Color.java
@@ -1,4 +1,36 @@
+public enum Color 
+{
+	//initializing constants
+	RED(255, 0, 0),
+	GREEN(0, 255, 0),
+	BLUE(0, 0, 255),
+	YELLOW(255, 255, 0),
+	CYAN(0, 255, 255),
+	MAGENTA(255, 0, 0);
+	
+	//variables for arguments of constants
+	private final int r;
+	private final int g;
+	private final int b;
+	
+	//Constructor
+	Color(int red, int green, int blue) 
+	{
+		r = red;
+		g = green;
+		b = blue;
+	}
 
-public enum Color {
+	//Getter's
+	public int getR() {
+		return r;
+	}
 
+	public int getG() {
+		return g;
+	}
+
+	public int getB() {
+		return b;
+	}
 }
